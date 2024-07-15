35d944df83cc904d1271f4a31f922d00de489777
Econsoft <matheushellf@gmail.com>
2024-07-15 15:15:30 -0300

    feature: carregando api id/images/names/types

--- app/src/main/java/com/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/pokedex/view/MainActivity.kt
@@ -3,28 +3,68 @@ package com.pokedex.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pokedex.api.model.PokemonRepository
 import com.pokedex.databinding.ActivityMainBinding
 import com.pokedex.domain.Pokemon
-import com.pokedex.domain.PokemonType
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var recyclerView: RecyclerView
+
+    var pokemons = emptyList<Pokemon?>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        recyclerView = binding.recyclerViewPokemons
 
-        val charmandar = Pokemon(
-            "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/004.png",
-            4, "charmander", listOf(PokemonType("Fire"))
-        )
-        val pokemons = listOf(
-            charmandar,charmandar,charmandar,charmandar,charmandar,charmandar,charmandar,charmandar
-        )
+        if (pokemons.isEmpty()) {
+            Thread(Runnable {
+                loadPokemons()
+            }).start()
+        } else {
+            loadRecyclerView()
+        }
+    }
 
-        val recyclerView = binding.recyclerViewPokemons
-        val layoutManager = LinearLayoutManager(this)
-        recyclerView.layoutManager = layoutManager
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun loadPokemons() {
+        val pokemonsApiResult = PokemonRepository.listPokemons()
+        pokemonsApiResult?.results?.let {
+            pokemons = it.map { pokemonResult ->
+                val number =
+                    pokemonResult.url
+                        .replace("https://pokeapi.co/api/v2/pokemon/", "")
+                        .replace("/", "").toInt()
+                val pokemonApiResult = PokemonRepository.getPokemon(number)
+
+                pokemonApiResult?.let {
+                    Pokemon(
+                        pokemonApiResult.id,
+                        pokemonApiResult.name,
+                        pokemonApiResult.types.map { type ->
+                            type.type
+                        }
+                    )
+                }
+            }
+            recyclerView.post {
+                loadRecyclerView()
+            }
+        }
+    }
+
+    private fun loadRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = PokemonAdapter(pokemons)
     }
 }
